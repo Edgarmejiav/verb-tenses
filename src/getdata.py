7d1df15ec5ac4eb7747f00fd272f7e912b005c87
edgarmejiav <legamejiav@gmail.com>
2024-07-30 15:26:51 -0500

    ✨ : github

--- src/getdata.py
+++ src/getdata.py
@@ -1,7 +1,6 @@
 import os
 import json
-from os import listdir
-from os.path import isfile, join, abspath
+from os.path import abspath
 
 
 def generate_md_list(directory, output_file):
@@ -46,13 +45,3 @@ def write_output_file(verb, content):
     with open(output_path, "w") as out_file:
         out_file.write(content)
 
-if __name__ == "__main__":
-    TEMPLATE_README = "../template.md"
-    DATA_DIR = abspath("../verbs")
-    raw_template_content = read_template(TEMPLATE_README)
-    generate_md_list(abspath("../verbs-md"), '../README.md')
-    for f in listdir(DATA_DIR):
-        if isfile(join(DATA_DIR, f)) and f.endswith(".json"):
-            json_path = join(DATA_DIR, f)
-            verb, raw_template = process_json_file(json_path, raw_template_content)
-            write_output_file(verb, raw_template)
---
+++ src/main.py
@@ -0,0 +1,14 @@
+from os import listdir
+from os.path import isfile, join, abspath
+from src.getdata import generate_md_list, read_template, process_json_file, write_output_file
+
+if __name__ == "__main__":
+    TEMPLATE_README = "../template.md"
+    DATA_DIR = abspath("../verbs")
+    raw_template_content = read_template(TEMPLATE_README)
+    generate_md_list(abspath("../verbs-md"), '../README.md')
+    for f in listdir(DATA_DIR):
+        if isfile(join(DATA_DIR, f)) and f.endswith(".json"):
+            json_path = join(DATA_DIR, f)
+            verb, raw_template = process_json_file(json_path, raw_template_content)
+            write_output_file(verb, raw_template)
