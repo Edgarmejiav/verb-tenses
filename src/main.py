69b15fe5b607042a4efb345e60458c0d13d2c9d9
edgarmejiav <legamejiav@gmail.com>
2024-07-30 15:40:27 -0500

    ✨ : path

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from getdata import generate_md_list, read_template, process_json_file, write_ou
 
 if __name__ == "__main__":
     TEMPLATE_README = "../template.md"
-    DATA_DIR = "../verbs"
+    DATA_DIR = abspath("../verbs")
     raw_template_content = read_template(TEMPLATE_README)
     generate_md_list("../verbs-md", '../README.md')
     for f in listdir(DATA_DIR):
